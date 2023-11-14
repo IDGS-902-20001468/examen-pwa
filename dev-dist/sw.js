3fd0612a4a127fba07ed1c45044e8c67b4a675b0
IDGS-902-20001468 <20001468@alumnos.utleon.edu.mx>
2023-11-13 20:53:55 -0600

    examen pwa react

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kf5pgm29148"
+    "revision": "0.9269l7jq89o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
